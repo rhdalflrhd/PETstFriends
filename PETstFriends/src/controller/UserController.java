10e982c4bf8fb6e614f84b8eaea2deb18e6f5649
rhdalflrhd <rhdalflrhd@gmail.com>
2018-08-28 10:32:53 +0900

    ryun

--- PETstFriends/src/controller/UserController.java
+++ PETstFriends/src/controller/UserController.java
@@ -1,16 +1,14 @@
 package controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-
-import service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;import org.springframework.stereotype.Controller;
+import service.UserServiceImpl;
 
 @Controller
 public class UserController {
 	
 	
 	@Autowired
-	UserService userService;
+	UserServiceImpl userService;
 }
 
 	
