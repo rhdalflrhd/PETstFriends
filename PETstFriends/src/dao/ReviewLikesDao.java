afed65cb72c3c4c26d0331fda7d1ebf58f9bb4ba
rhdalflrhd <rhdalflrhd@gmail.com>
2018-08-28 09:41:39 +0900

    11

--- PETstFriends/src/dao/ReviewLikesDao.java
+++ PETstFriends/src/dao/ReviewLikesDao.java
@@ -1,5 +1,15 @@
 package dao;
 
+import java.util.HashMap;
+import java.util.List;
+
+import model.MeetingLikes;
+
 public interface ReviewLikesDao {
+	public boolean insertReviewLikes(MeetingLikes rLikes);
+	public boolean deleteReviewLikes(HashMap<String, Object> param);
+	public List<MeetingLikes> selectAllReviewLikes(HashMap<String, Object> param);
+	public List<MeetingLikes> selectOneReviewLikes(HashMap<String, Object> param);
+	public int getReviewLikesCount(HashMap<String, Object> param);
 
 }
