e0a23f34cbb663388bbaa33dc63b7feb8f7ee5f5
rhdalflrhd <rhdalflrhd@gmail.com>
2018-08-28 09:32:31 +0900

    용준고침

--- PETstFriends/src/dao/UserDao.java
+++ PETstFriends/src/dao/UserDao.java
@@ -1,10 +1,18 @@
 package dao;
 
+import java.util.HashMap;
+import java.util.List;
+
+import model.Pet;
 import model.User;
 
 public interface UserDao {
-
-	public boolean updateUser(User user);
 	
+	public User selectUserNn(String user_nickname);
 	public User selectUserId(String user_id);
+	public int insertPet(Pet pet); //회원가입
+	public int insertUser(User user); //회원가입
+	public List<User> selectUserAll(HashMap<String, Object> params);
+	public int getCount(String keyword);
+	public int updateUser(User user);
 }
